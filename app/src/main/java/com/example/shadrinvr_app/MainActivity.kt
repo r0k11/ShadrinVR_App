d2183849a2f2f2504046a7ca5e2fac7d4db55cf7
r0k11 <shad.vad@yandex.ru>
2025-05-31 14:34:27 +0300

    Change color

--- app/src/main/java/com/example/shadrinvr_app/MainActivity.kt
+++ app/src/main/java/com/example/shadrinvr_app/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.shadrinvr_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Color
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var squareView: View
+    private lateinit var changeColorButton: Button
+
+    private var currentColorIndex = 0
+    private val colors = arrayOf(Color.parseColor("#964B00"), Color.GRAY, Color.RED)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        squareView = findViewById(R.id.squareView)
+        changeColorButton = findViewById(R.id.changeColorButton)
+
+        changeColorButton.setOnClickListener {
+            changeSquareColor()
+        }
+    }
+
+    private fun changeSquareColor() {
+        currentColorIndex = (currentColorIndex + 1) % colors.size
+        squareView.setBackgroundColor(colors[currentColorIndex])
     }
 }

e6c2261386f10f327f125ba750c05563ffdc31b7
r0k11 <shad.vad@yandex.ru>
2025-05-31 15:27:27 +0300

    Change number

--- app/src/main/java/com/example/shadrinvr_app/MainActivity.kt
+++ app/src/main/java/com/example/shadrinvr_app/MainActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.graphics.Color
 import android.view.View
 import android.widget.Button
+import android.widget.EditText;
 
 class MainActivity : AppCompatActivity() {
     private lateinit var squareView: View
     private lateinit var changeColorButton: Button
-
-    private var currentColorIndex = 0
     private val colors = arrayOf(Color.parseColor("#964B00"), Color.GRAY, Color.RED)
+
+    private lateinit var numberEditText: EditText
+    private lateinit var incrementButton: Button
+    private var currentColorIndex = 0
+    private var currentNumber = 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +23,17 @@ class MainActivity : AppCompatActivity() {
 
         squareView = findViewById(R.id.squareView)
         changeColorButton = findViewById(R.id.changeColorButton)
+        numberEditText = findViewById(R.id.numberEditText)
+        incrementButton = findViewById(R.id.incrementButton)
+
+        numberEditText.setText(currentNumber.toString())
 
         changeColorButton.setOnClickListener {
             changeSquareColor()
+        }
+
+        incrementButton.setOnClickListener {
+            incrementNumber()
         }
     }
 
@@ -29,4 +41,13 @@ class MainActivity : AppCompatActivity() {
         currentColorIndex = (currentColorIndex + 1) % colors.size
         squareView.setBackgroundColor(colors[currentColorIndex])
     }
+
+    private fun incrementNumber() {
+        currentNumber += 6
+        if (currentNumber > 36) {
+            currentNumber = 6
+        }
+        numberEditText.setText(currentNumber.toString())
+    }
+
 }
